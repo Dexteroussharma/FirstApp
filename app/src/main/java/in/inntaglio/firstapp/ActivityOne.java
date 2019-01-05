1a006b817dfcc190fa12e3ab2deb5bf49e236e41
Dexteroussharma <navin5usharma@gmail.com>
2019-01-05 20:51:38 +0530

    Final Commit

---
+++ app/src/main/java/in/inntaglio/firstapp/ActivityOne.java
@@ -0,0 +1,18 @@
+package in.inntaglio.firstapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by navin on 05,January,2019.
+ * Company: Inntaglio
+ * Email: Dexteroussharma@gmail.com
+ */
+public class ActivityOne extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+}
